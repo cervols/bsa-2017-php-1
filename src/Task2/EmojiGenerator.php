46f6466f782189e2dfc7b96774d911e963458aa2
cervols <tania.zavediuk@gmail.com>
2017-05-26 07:25:51 +0300

    Task2 done

--- src/Task2/EmojiGenerator.php
+++ src/Task2/EmojiGenerator.php
@@ -4,8 +4,19 @@ namespace BinaryStudioAcademy\Task2;
 
 class EmojiGenerator
 {
+	const EMOJI = [
+		"\u{1f600}",
+		"\u{1f603}",
+		"\u{1f604}",
+		"\u{1f601}",
+		"\u{1f606}",
+		"\u{1f605}",
+		"\u{1f602}"
+	];
     public function generate()
     {
         //TODO: Implement emoji generator using yield
+		foreach (self::EMOJI as $em)
+			yield $em;
     }
 }
